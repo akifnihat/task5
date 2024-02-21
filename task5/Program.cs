e1542b9795450e4bfedb1ce07e1cab5b08736273
akifnihat <akifst@code.edu.az>
2024-02-21 16:36:23 +0400

    task5

---
+++ task5/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+#region task5
+//int[] arr = { 156, 345, 4563, 1234, 12345, 65743, 389476 };
+//int sum3 = 0;
+//int mult4 = 1;
+//int sum4 = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] >= 99 && arr[i] <= 1000)
+//    {
+
+//        sum3 += arr[i];
+//    }
+//    else if (arr[i] >= 1000 && arr[i] <= 10000)
+//    {
+//        mult4 *= arr[i];
+//        sum4 += arr[i];
+//    }
+//}
+//int yekun = (sum3 + mult4) * 10 + 7;
+//int sum5 = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] >= 10000 && arr[i] <= 100000)
+//    {
+//        sum5 += arr[i];
+//    }
+//}
+//int yekun2 = sum5 + yekun;
+//int hasil3 = 1;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] >= 99 && arr[i] <= 1000)
+//    {
+//        hasil3 *= arr[i];
+//    }
+//}
+//int netice = yekun2 - ((hasil3 * 10) + 1);
+//int hasil6 = 1;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] >= 100000 && arr[i] <= 1000000)
+//    {
+//        hasil6 *= arr[i];
+//    }
+//}
+//int netice2 = netice + hasil6;
+//int proses3 = netice2 - (sum3 + sum4);
+//int faiz18 = proses3 * 18 / 100;
+//int faiz3 = faiz18 * 3 / 100;
+//int faiz1 = faiz3 * 1 / 100;
+//Console.WriteLine(faiz1 + sum5);
+
+#endregion
